aa115e19648fb789ebfe27f0c02552c22b3f6ab5
Alvaro Garcia <derecerca@gmail.com>
2018-07-02 06:31:44 +0200

    Red: transfer to a not-secure account

--- src/test/kotlin/com/example/etude/statemachine/FiniteStateMachineShould.kt
+++ src/test/kotlin/com/example/etude/statemachine/FiniteStateMachineShould.kt
@@ -29,6 +29,20 @@ class FiniteStateMachineShould {
         differentBalance(from, to)
     }
 
+    @Test
+    fun `secure to a not-secure account`() {
+        val transactionStateMachine = Transfer.aNew()
+        val from = Account.secure()
+        val to = Account.notSecure()
+        createBalance(from, to)
+
+        transactionStateMachine.transfer(1000, "rent", from, to)
+        sameBalance(from, to)
+        from.userConfirmOutgoing("1234")
+
+        differentBalance(from, to)
+    }
+
     private fun differentBalance(from: Account, to: Account) {
         assertThat(from.balance()).isNotEqualTo(balances["from"])
         assertThat(to.balance()).isNotEqualTo(balances["to"])
